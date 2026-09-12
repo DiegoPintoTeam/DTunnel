6c41cc98a8b8ac163f9a456760ae4cca77036f3e
Diego Pinto (Team) <120763964+DiegoPintoTeam@users.noreply.github.com>
2026-09-12 13:44:15 -0500

    Update

--- environment.d.ts
+++ environment.d.ts
@@ -6,6 +6,13 @@ declare global {
       JWT_SECRET_KEY: string;
       JWT_SECRET_REFRESH: string;
       NODE_ENV: 'development' | 'production';
+      R2_ACCOUNT_ID?: string;
+      R2_ACCESS_KEY_ID?: string;
+      R2_SECRET_ACCESS_KEY?: string;
+      R2_BUCKET_NAME?: string;
+      R2_PUBLIC_DOMAIN?: string;
+      DTUNNEL_SUPERVISED?: string;
+      pm_id?: string;
     }
   }
 }
