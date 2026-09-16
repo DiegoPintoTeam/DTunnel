b393c21fb789a6c4348d926075164dc7a2b243ae
Diego Pinto (Team) <120763964+DiegoPintoTeam@users.noreply.github.com>
2026-09-15 19:12:04 -0500

    Update

--- frontend/public/static/js/app_config/modals/export.js
+++ frontend/public/static/js/app_config/modals/export.js
@@ -1,26 +1,11 @@
-class ConfigAppExportFile {
-    constructor(config) {
-        this.config = config;
-    }
-
-    async export() {
-        const blob = new Blob([this.config], { type: 'application/json' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'app_config.json';
-        a.click();
-        URL.revokeObjectURL(url);
-    }
-}
-
-class ConfigAppExportFactory {
-    static create(type, config) {
-        switch (type) {
-            case 'FILE':
-                return new ConfigAppExportFile(config);
-        }
-    }
+const exportConfigToFile = (config) => {
+    const blob = new Blob([config], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = 'app_config.json';
+    anchor.click();
+    URL.revokeObjectURL(url);
 }
 
 const convertConfigToExport = (config) => {
@@ -45,15 +30,9 @@ class AppConfigExportModal {
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <textarea class="form-control mb-3 d-none" rows="10"></textarea>
                 <div class="__preview d-flex align-items-center justify-content-center mb-3"></div>
-                <div class="__spinner justify-content-center p-3 d-none">
-                    <div class="spinner-border p-3" role="status">
-                        <span class="visually-hidden">Loading...</span>
-                    </div>
-                </div>
                 <div class="d-grid">
-                    <button type="button" class="btn btn-dark w-100" data-type="FILE">ARCHIVO</button>
+                    <button type="button" class="btn btn-dark w-100 btn__export">ARCHIVO</button>
                 </div>
             </div>
         </div>
@@ -67,48 +46,26 @@ class AppConfigExportModal {
         this._element.innerHTML = this.__html;
 
         this._root = this._element.querySelector('.modal-body');
-        this._root.querySelector('textarea').value = convertConfigToExport(config);
+        this.config = convertConfigToExport(config);
 
         this.modal = new bootstrap.Modal(this._element);
-        this._root.querySelector('button[data-type="FILE"]').addEventListener('click', () => this.export('FILE'));
+        this._root.querySelector('.btn__export').addEventListener('click', () => this.export());
 
         this._preview = this._root.querySelector('.__preview');
         this._preview.appendChild(app.element);
     }
 
-    showLoading() {
-        this._root.querySelector('.__spinner').classList.remove('d-none');
-        this._root.querySelector('.__spinner').classList.add('d-flex');
-    }
-
-    hideLoading() {
-        this._root.querySelector('.__spinner').classList.add('d-none');
-        this._root.querySelector('.__spinner').classList.remove('d-flex');
-    }
-
-    async export(type) {
-        showToastInfo('Exportando configuración...');
-        this.showLoading();
-
+    export() {
         try {
-            const config = this._root.querySelector('textarea').value;
-            const exportConfig = ConfigAppExportFactory.create(type, config);
-            await exportConfig.export();
-
+            exportConfigToFile(this.config);
             showToastSuccess('Configuración exportada correctamente!');
         } catch (error) {
             showToastError(error.message);
-        } finally {
-            this.hideLoading();
         }
     }
 
     show() {
         this.modal.show();
-    }
-
-    hide() {
-        this.modal.hide();
     }
 }
 
